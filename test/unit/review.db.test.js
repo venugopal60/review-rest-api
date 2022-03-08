4816b2a8a874880a5f8610e08cabd30b3625089e
Venugopal Parala <vparal11@in.ibm.com>
2022-03-08 09:32:44 +0530

    file format

--- test/unit/review.db.test.js
+++ test/unit/review.db.test.js
@@ -1,23 +1,31 @@
-const { expect } = require('chai');
 const mongoUnit = require('mongo-unit');
+const { expect } = require('chai');
+const sortBy = require('lodash/sortBy');
+const DbConnection = require('../../src/models/db.connector');
 const testData = require('../mocks/review.data.mock');
 const ReviewSchema = require('../../src/models/review.schema');
 const requestData = require('../mocks/review.request.mock');
-const DbConnection = require('../../src/models/db.connector')
-const sortBy = require('lodash/sortBy');
+
+
+
 describe('Review Model', () => {
   let connection;
   let ReviewModel;
+
   before(async () => {
     connection = await DbConnection.openDbConnection(process.env.DATABASE_URL);
     ReviewModel = ReviewSchema.createModel(connection);
   })
+
   beforeEach(async () => { await mongoUnit.load(testData); })
+  
   afterEach(() => mongoUnit.drop())
 
   it('Test should get average monthly rating by review source and average rating for 2017-02 should be 2', async () => {
+  
     // return await ReviewModel.Review.paginate({})
     // return await ReviewModel.Review.find({})
+  
     const result = await ReviewModel.aggregate(requestData.monthlyRatings);
 
     expect(result).to.be.an('array');
@@ -28,18 +36,8 @@ describe('Review Model', () => {
       x.report = sortBy(x.report, ['review_source']);
     });
 
-    expect(result).to.have.deep.members(expectedResult);
-
-    // expect(result[0]).to.have.own.property('review_month');
-    // expect(result[0]).to.have.own.property('report');
-    // expect(result[0].report[0]).to.have.own.property('review_source');
-    // expect(result[0].report[0]).to.have.own.property('average_rating');
-
-    // expect(result[0].review_month).to.equal('2020-01');
-
-    // expect(result[10].review_month).to.equal('2017-05');
-    // expect(result[10].report[0].review_source).to.equal('GooglePlayStore');
-    // expect(result[10].report[0].average_rating).to.equal(2);    
+    expect(result).to.have.deep.members(expectedResult);   
+    
   });
 
 });
